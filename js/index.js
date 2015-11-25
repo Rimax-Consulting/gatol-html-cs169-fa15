1cda33ef0bf4f03ec81bedd859641d5dfec1f556
Olivia Chen <odevchen@gmail.com>
2015-11-25 12:37:38 -0800

    Modify and Clean Home Page
    - Add form to index.html to show field validation
    - Modify index.js login to only submit request if valid
    - Clean code to use $ selector instead of find()

--- js/index.js
+++ js/index.js
@@ -11,74 +11,75 @@ var GameAThon = (function () {
     var attachCreateHandler = function (e) {
 
         // hide the login screen initially
-        loginContainer.find("#login_screen").hide();
+        $("#login_screen").hide();
 
         // hide the trainer login screen initially
-        loginContainer.find("#trainer_login_screen").hide();
+        $("#trainer_login_screen").hide();
 
         // hide the register screen initially
-        loginContainer.find("#register_screen").hide();
+        $("#register_screen").hide();
 
         // hide the register trainer screen initially
-        loginContainer.find('#register_trainer_screen').hide();
+        $('#register_trainer_screen').hide();
 
         // hide the forgot screen initially
-        loginContainer.find('#forgot_screen').hide();
+        $('#forgot_screen').hide();
 
         // hide the trainer forgot screen initially
-        loginContainer.find("#trainer_forgot_screen").hide();
+        $("#trainer_forgot_screen").hide();
 
         // hide the login title
-        loginContainer.find('#login_title').hide();
+        $('#login_title').hide();
 
 
         // forgot password 
         loginContainer.on('click', '#user_forgot', function (e) {
-            loginContainer.find('#login_title').html('');
-            loginContainer.find('#login_title').append('Reset Password');
-            loginContainer.find('#login_title').show();
-            loginContainer.find('#login_screen').hide();
-            loginContainer.find('#forgot_screen').show();
+            $('#login_title').html('');
+            $('#login_title').append('Reset Password');
+            $('#login_title').show();
+            $('#login_screen').hide();
+            $('#forgot_screen').show();
         });
 
         // forgot password back button
         loginContainer.on('click', '#forgot_back', function (e) {
-            loginContainer.find('#login_title').html('');
-            loginContainer.find('#login_title').append('Login');
-            loginContainer.find('#login_title').show();
-            loginContainer.find('#forgot_screen').hide();
-            loginContainer.find('#login_screen').show();
+            $('#login_title').html('');
+            $('#login_title').append('Login');
+            $('#login_title').show();
+            $('#forgot_screen').hide();
+            $('#login_screen').show();
         });
 
         // forgot trainer password 
         loginContainer.on('click', '#trainer_forgot', function (e) {
-            loginContainer.find('#login_title').html('');
-            loginContainer.find('#login_title').append('Reset Trainer Password');
-            loginContainer.find('#login_title').show();
-            loginContainer.find('#trainer_login_screen').hide();
-            loginContainer.find('#trainer_forgot_screen').show();
+            $('#login_title').html('');
+            $('#login_title').append('Reset Trainer Password');
+            $('#login_title').show();
+            $('#trainer_login_screen').hide();
+            $('#trainer_forgot_screen').show();
         });
 
         // forgot trainer password back button
         loginContainer.on('click', '#trainer_forgot_back', function (e) {
-            loginContainer.find('#login_title').html('');
-            loginContainer.find('#login_title').append('Trainer Login');
-            loginContainer.find('#login_title').show();
-            loginContainer.find('#trainer_forgot_screen').hide();
-            loginContainer.find('#trainer_login_screen').show();
+            $('#login_title').html('');
+            $('#login_title').append('Trainer Login');
+            $('#login_title').show();
+            $('#trainer_forgot_screen').hide();
+            $('#trainer_login_screen').show();
         });
 
 
         // student login
         loginContainer.on('click', '#student_login', function (e) {
-            loginContainer.find('#login_title').html('');
-            loginContainer.find('#login_title').append('Login');
-            loginContainer.find('#login_title').show();
-            loginContainer.find("#select_screen").hide();
-            loginContainer.find("#login_screen").show();
+            $('#login_title').html('');
+            $('#login_title').append('Login');
+            $('#login_title').show();
+            $("#select_screen").hide();
+            $("#login_screen").show();
         });
 
         loginContainer.on('click', '#sign_in', function (e) {
+            e.preventDefault();
             // do user sign in logic
 
             //if (getCookie("auth_token") != "") {
@@ -88,34 +89,38 @@ var GameAThon = (function () {
 
             var creds = {} // prepare credentials for passing into backend
 
-            creds.email = loginContainer.find('#user_email').val();
-            creds.password = loginContainer.find('#user_password').val();
+            //creds.email = $('#user_email').val();
+            //creds.password = $('#user_password').val();
+            
+            if (checkLoginValid(creds, false)) {
 
-            var onSuccess = function (data) {
-                //alert('successfully logged in as user, auth token is: ' + data.auth_token);
-                setCookie("auth_token", data.auth_token);
-                setCookie("username", data.username);
-                setCookie("trainer", "false"); // determines whether or not I am a trainer
-                if (inDev) {
-                    location.href = devUrl + 'dashboard.html';
-                } else {
-                    location.href = 'http://allenyu94.github.io/gatol-html/dashboard';
-                }
-            };
-            var onFailure = function (data) {
-                //console.error('failure to login as user');
-                consoleError(data);
-                msg = extractJSONFailMsg(data)
-                displayError('Login Failed! ' + msg +' Please try again.', '#login_screen');
-            };
+                var onSuccess = function (data) {
+                    //alert('successfully logged in as user, auth token is: ' + data.auth_token);
+                    setCookie("auth_token", data.auth_token);
+                    setCookie("username", data.username);
+                    setCookie("trainer", "false"); // determines whether or not I am a trainer
+                    if (inDev) {
+                        location.href = devUrl + 'dashboard.html';
+                    } else {
+                        location.href = 'http://allenyu94.github.io/gatol-html/dashboard';
+                    }
+                };
+                var onFailure = function (data) {
+                    //console.error('failure to login as user');
+                    consoleError(data);
+                    msg = extractJSONFailMsg(data)
+                    displayError('Login Failed! ' + msg +' Please try again.', '#login_screen');
+                };
 
-            url = '/api/sessions'
-            console.log(creds);
-            makePostRequest(url, creds, onSuccess, onFailure);
+                url = '/api/sessions'
+                console.log(creds);
+                makePostRequest(url, creds, onSuccess, onFailure);
+            }
 
         });
 
         loginContainer.on('click', '#trainer_sign_in', function (e) {
+            e.preventDefault();
             // do trainer sign in logic
 
             //if (getCookie("auth_token") != "") {
@@ -125,27 +130,30 @@ var GameAThon = (function () {
 
             var creds = {} // prepare credentials for passing into backend
 
-            creds.email = loginContainer.find('#trainer_email').val();
-            creds.password = loginContainer.find('#trainer_password').val();
+            //creds.email = $('#trainer_email').val();
+            //creds.password = $('#trainer_password').val();
 
-            var onSuccess = function (data) {
-                //alert('successfully logged in as trainer, auth token is: ' + data.auth_token);
-                setCookie("auth_token", data.auth_token);
-                setCookie("username", data.username);
-                setCookie("trainer", "true");
-                if (inDev) {
-                    location.href = devUrl + 'dashboard.html';
-                } else {
-                    location.href = 'http://allenyu94.github.io/gatol-html/dashboard';
-                }
-            };
-            var onFailure = function (data) {
-                consoleError(data);
-                displayError('Login Failed! Please try again.', '#trainer_login_screen')
-            };
+            if (checkLoginValid(creds, true)) {
 
-            url = '/api/sessions'
-            makePostRequest(url, creds, onSuccess, onFailure);
+                var onSuccess = function (data) {
+                    //alert('successfully logged in as trainer, auth token is: ' + data.auth_token);
+                    setCookie("auth_token", data.auth_token);
+                    setCookie("username", data.username);
+                    setCookie("trainer", "true");
+                    if (inDev) {
+                        location.href = devUrl + 'dashboard.html';
+                    } else {
+                        location.href = 'http://allenyu94.github.io/gatol-html/dashboard';
+                    }
+                };
+                var onFailure = function (data) {
+                    consoleError(data);
+                    displayError('Login Failed! Please try again.', '#trainer_login_screen')
+                };
+
+                url = '/api/sessions'
+                makePostRequest(url, creds, onSuccess, onFailure);
+            }
         });
 
         loginContainer.on('click', '#logout', function (e) {
@@ -155,68 +163,68 @@ var GameAThon = (function () {
 
         // login back
         loginContainer.on('click', '#login_back', function (e) {
-            backToMain(loginContainer, '#login_screen');
+            backToMain('#login_screen');
         });
 
         // trainer login back
         loginContainer.on('click', '#trainer_login_back', function (e) {
-            backToMain(loginContainer, '#trainer_login_screen');
+            backToMain('#trainer_login_screen');
         });
 
 
         // trainer login
         loginContainer.on('click', '#trainer_login', function (e) {
-            loginContainer.find('#login_title').html('');
-            loginContainer.find('#login_title').append('Trainer Login');
-            loginContainer.find('#login_title').show();
-            loginContainer.find('#select_screen').hide();
-            loginContainer.find("#trainer_login_screen").show();
+            $('#login_title').html('');
+            $('#login_title').append('Trainer Login');
+            $('#login_title').show();
+            $('#select_screen').hide();
+            $("#trainer_login_screen").show();
             resetAllErrors();
         });
 
         // register 
         loginContainer.on('click', '#register', function (e) {
-            loginContainer.find('#login_title').html('');
-            loginContainer.find('#login_title').append('Register As Student');
-            loginContainer.find('#login_title').show();
-            loginContainer.find('#select_screen').hide();
-            loginContainer.find('#register_screen').show();
+            $('#login_title').html('');
+            $('#login_title').append('Register As Student');
+            $('#login_title').show();
+            $('#select_screen').hide();
+            $('#register_screen').show();
             resetAllErrors();
         });
 
         // register back
         loginContainer.on('click', '#register_back', function (e) {
-            backToMain(loginContainer, '#register_screen');
+            backToMain('#register_screen');
         });
 
         // register trainer
         loginContainer.on('click', '#go_register_trainer', function (e) {
-            loginContainer.find('#login_title').html('');
-            loginContainer.find('#login_title').append('Register As Trainer');
-            loginContainer.find('#login_title').show();
-            loginContainer.find('#select_screen').hide();
-            loginContainer.find('#register_trainer_screen').show();
+            $('#login_title').html('');
+            $('#login_title').append('Register As Trainer');
+            $('#login_title').show();
+            $('#select_screen').hide();
+            $('#register_trainer_screen').show();
             resetAllErrors();
         });
 
         // register trainer back
         loginContainer.on('click', '#register_trainer_back', function (e) {
-            backToMain(loginContainer, '#register_trainer_screen');
+            backToMain('#register_trainer_screen');
         });
 
         loginContainer.on('click', '#register_user', function (e) {
             var creds = {} // prepare credentials for passing into backend
 
-            if (loginContainer.find('#register_password').val() != loginContainer.find('#register_confirm_password').val()) {
+            if ($('#register_password').val() != $('#register_confirm_password').val()) {
                 //  alert('password does not match');
                 displayError('Passwords do not match! Please re-enter.', '#register_screen')
                 return;
             }
 
-            creds.email = loginContainer.find('#register_email').val();
-            creds.username = loginContainer.find('#username').val();
-            creds.password = loginContainer.find('#register_password').val();
-            creds.confirm_password = loginContainer.find('#register_confirm_password').val();
+            creds.email = $('#register_email').val();
+            creds.username = $('#username').val();
+            creds.password = $('#register_password').val();
+            creds.confirm_password = $('#register_confirm_password').val();
 
             var onSuccess = function (data) {
                 alert('successfully registered user');
@@ -245,16 +253,16 @@ var GameAThon = (function () {
         loginContainer.on('click', '#register_trainer', function (e) {
             var creds = {} // prepare credentials for passing into backend
 
-            if (loginContainer.find('#register_trainer_password').val() != loginContainer.find('#register_trainer_confirm_password').val()) {
+            if ($('#register_trainer_password').val() != $('#register_trainer_confirm_password').val()) {
                 //alert('password does not match');
                 displayError('Passwords do not match! Please re-enter.', '#register_screen')
                 return;
             }
 
-            creds.email = loginContainer.find('#register_trainer_email').val();
-            creds.username = loginContainer.find('#trainer_username').val();
-            creds.password = loginContainer.find('#register_trainer_password').val();
-            creds.confirm_password = loginContainer.find('#register_trainer_confirm_password').val();
+            creds.email = $('#register_trainer_email').val();
+            creds.username = $('#trainer_username').val();
+            creds.password = $('#register_trainer_password').val();
+            creds.confirm_password = $('#register_trainer_confirm_password').val();
 
             var onSuccess = function (data) {
                 alert('successfully registered trainer');
@@ -328,9 +336,31 @@ function extractJSONFailMsg(data) {
 
 }
 
-function backToMain(loginContainer, currentScreen) {
-    loginContainer.find('#login_title').hide();
-    loginContainer.find(currentScreen).hide();
-    loginContainer.find('#select_screen').show();
+function backToMain(currentScreen) {
+    $('#login_title').hide();
+    $(currentScreen).hide();
+    $('#select_screen').show();
     resetAllErrors();
+}
+
+function checkLoginValid(creds, is_trainer) {
+    user = 'user'
+    if (is_trainer) {
+        user = 'trainer'
+    }
+    email = $('#' + user + '_email');
+    password = $('#' + user + '_password');
+    valid  = email[0].checkValidity() && password[0].checkValidity()
+    if (valid) {
+        creds.email = email.val();
+        creds.password = password.val();
+    }
+
+    return valid
+    //if 
+    
+}
+
+function checkRegisterFields() {
+    
 }
